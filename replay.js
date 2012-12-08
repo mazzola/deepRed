983f6625dfb487b7a74898ac04b5b198aab93fca
travis <tcw48@cornell.edu>
2012-12-08 16:35:24 -0500

    removed some debug print statemenst

--- replay.js
+++ replay.js
@@ -16,7 +16,7 @@ function replay(m){
 }
 
 function replayMove(){
-	if (replayMoves.length <= 10){
+	if (replayMoves.length <= 16){
 		console.log("replay done");
 		clearInterval(sendKey);
 		setTimeout("runAI()", 200);
